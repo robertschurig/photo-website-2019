6de63852b51218b187c5ccc1fd77500a86798fae
Robert Schurig <aqua-relax@gmx.de>
2018-10-20 01:49:42 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rsc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'photo-app';
+}
